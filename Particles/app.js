45811195bd581443accb112943f12b3230cc9f9c
yhaw12 <yooxing11@gmail.com>
2023-09-12 23:35:47 +0000

    mobile update

---
+++ Particles/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'Particles/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./**/*.{html,js}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+    },
+    screens: {
+
+      'mobile': '420px',
+      // => @media (min-width: 420px) { ... }
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+}
 
